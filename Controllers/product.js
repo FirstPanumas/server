c6d3aadc6aa1b47dd0e5038b32aa7c8b8b78f473
panumas20 <panumas20062540@gmail.com>
2023-09-13 13:27:02 +0700

    update from mac

--- Controllers/product.js
+++ Controllers/product.js
@@ -1,10 +1,24 @@
+const Product = require("../Models/Product");
+
 exports.read = async (req, res) => {
-  res.send("Hello product Endpoint");
+  try {
+    const id = req.params.id;
+    const producted = await Product.findOne({
+      _id: id,
+    }).exec();
+    res.send(producted);
+  } catch {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
 };
 
 exports.list = async (req, res) => {
   try {
-    res.send("Hello list");
+    const producted = await Product.find(
+      {}
+    ).exec();
+    res.send(producted);
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error");
@@ -13,7 +27,12 @@ exports.list = async (req, res) => {
 
 exports.create = async (req, res) => {
   try {
-    res.send("Hello list");
+    console.log(req.body);
+    const producted = await Product(
+      req.body
+    ).save();
+
+    res.send(producted);
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error");
@@ -21,7 +40,15 @@ exports.create = async (req, res) => {
 };
 exports.update = async (req, res) => {
   try {
-    res.send("Hello list");
+    const id = req.params.id;
+    const updated =
+      await Product.findOneAndUpdate(
+        {_id: id},
+        req.body,
+        {new: true}
+      ).exec();
+
+    res.send(updated);
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error");
@@ -29,7 +56,12 @@ exports.update = async (req, res) => {
 };
 exports.remove = async (req, res) => {
   try {
-    res.send("Hello list");
+    const id = req.params.id;
+    const removed =
+      await Product.findOneAndDelete({
+        _id: id,
+      }).exec();
+    res.send(removed);
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error");
--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const morgan = require("morgan");
 const cors = require("cors");
 const bodyParse = require("body-parser");
 
-const { readdirSync } = require("fs");
+const {readdirSync} = require("fs");
 const productRouters = require("./Routes/product");
 const authgRouters = require("./Routes/auth");
 
+const connetDB = require("./Config/db");
+
 const app = express();
+
+connetDB();
+
 app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParse.json({ limit: "10mb" }));
+app.use(bodyParse.json({limit: "10mb"}));
 
 const port = 8000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.send("Hello World!")
+);
 app.use("/api", productRouters);
 app.use("/api", authgRouters);
 
@@ -24,4 +31,8 @@ readdirSync("./Routes").map((r) => {
   app.use("./api", require("./Routes/" + r));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(
+    `Example app listening on port ${port}!`
+  )
+);
